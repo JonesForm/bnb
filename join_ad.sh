fbd7cc6a99ae4fa4ee70c09a19fec3f37a4c0ea0
Lee Jones <60527755+JonesForm@users.noreply.github.com>
2025-05-12 00:25:12 +0100

    Add files via upload

--- join_ad.sh
+++ join_ad.sh
@@ -8,7 +8,7 @@ COMPUTER_OU="OU=BP_Computers,DC=bigpurple,DC=com"
 
 # Packages incase not installed
 echo "Installing required packages..."
-sudo dnf install -y realmd sssd oddjob oddjob-mkhomedir adcli samba-common-tools krb5-workstation
+sudo dnf install -y realmd sssd oddjob oddjob-mkhomedir adcli samba-common-tools krb5-workstation gvfs-smb smbclient
 
 # Discover the server
 echo "Discovering Domain..."
@@ -19,16 +19,19 @@ echo "Joining the domain"
 echo "${ADMIN_PASSWORD}" | sudo realm join --user="${JOIN_USER}" --computer-ou="${COMPUTER_OU}" "${DOMAIN}"
 
 if [ $? -eq 0 ]; then
-  echo "Successfully joined the domain: ${DOMAIN} and placed in OU: ${COMPUTER_OU}"
+    echo "Successfully joined the domain: ${DOMAIN} and placed in OU: ${COMPUTER_OU}"
 
-  # enable authentication
-  echo "Configuring authentication..."
-  sudo authselect select sssd with-mkhomedir -w --force
+    # enable authentication
+    echo "Configuring authentication..."
+    sudo authselect select sssd with-mkhomedir -w --force
+    sudo systemctl enable --now oddjobd
+    sudo systemctl enable --now sssd
+    sudo systemctl start sssd
 
-  echo "joined ${DOMAIN}"
+    echo "joined ${DOMAIN}"
 else
-  echo "Failed"
-  exit 1
+    echo "Failed"
+    exit 1
 fi
 
 exit 0
